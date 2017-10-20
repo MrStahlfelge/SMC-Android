f3eda8beba767f3f87b5d1b7e6d899603b623276
Benjamin Schulte <bs@golfgl.de>
2017-10-20 23:52:34 +0200

    Sprites und MovingPlatform auf Static-Atlas umgesetzt

--- smc-level-parser/src/main/java/rs/papltd/smc/smc_level_converter/Const.java
+++ smc-level-parser/src/main/java/rs/papltd/smc/smc_level_converter/Const.java
@@ -4,4 +4,6 @@ public class Const
 {
 	public static final String dataRoot = "C:/Users/Benni/Documents/Eigene Dokumente/game/smc/smc-org-data/pixmaps";
 	public static final String PROJECT_PATH = "C:/Users/Benni/Documents/Eigene Dokumente/game/smc";
+
+	public static final String UNPACKED_PATH = "C:/Users/Benni/Documents/Eigene Dokumente/game/smc/drafts/unpacked";
 }
